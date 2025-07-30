04e5956072954edbe4b5166dd59c7b388088fd97
Buraka12 <taciburak44@gmail.com>
2025-07-31 00:13:42 +0300

    Mainmenu ui added
    main menu buttons , demo settings, and demo background added

---
+++ scripts/main_menu.gd
@@ -0,0 +1,28 @@
+extends Control
+
+@onready var main_buttons: VBoxContainer = $Main_buttons
+@onready var settings_menu: Panel = $SettingsMenu
+
+func _ready() -> void:
+		main_buttons.visible = true
+		settings_menu.visible = false
+
+
+#            --Main buttons--
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/character_body_2d.tscn")
+
+func _on_settings_pressed() -> void:
+	main_buttons.visible = false
+	settings_menu.visible = true
+	
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+	
+	
+#        --Settings menu--
+
+func _on_back_button_pressed() -> void:
+	main_buttons.visible = true
+	settings_menu.visible = false
