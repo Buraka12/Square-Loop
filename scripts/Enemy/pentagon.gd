00a8d0c7463c88a64669e75a9886a745fa7ed772
Metehan <mcgamer0700@gmail.com>
2025-07-31 20:51:35 +0300

    Pentagon Anim and Shoot

--- scripts/Enemy/pentagon.gd
+++ scripts/Enemy/pentagon.gd
@@ -1,1 +1,76 @@
 extends CharacterBody2D
+
+@onready var area : Node2D = $".."
+@onready var player : CharacterBody2D = area.find_child("Player")
+
+@onready var bulletscene : PackedScene = load("res://scenes/enemy_bullet.tscn")
+var can_shoot : bool = true
+var fire_rate : float = 1
+var tab : float = 1000.0
+
+var health : float = 100
+var max_speed : float = 200.0
+var clutch : float = 200.0
+var SPEED : float = 0.0
+
+enum states {STOP,FOLLOW,FIRE}
+var state : states
+
+func _ready() -> void:
+	state = states.FOLLOW
+
+func _physics_process(delta: float) -> void:
+	look_at_player()
+	if state != states.STOP:
+		follow_player(delta)
+	else:
+		SPEED = 0
+		velocity = Vector2(0,0)
+	
+	if can_shoot:
+		can_shoot = false
+		var direction = (player.global_position-global_position).normalized()
+		velocity = -direction*tab
+		SPEED = 0
+		$Timer.start(1/fire_rate)
+		fire(direction)
+	
+	move_and_slide()
+
+func follow_player(delta):
+	var direction = (player.global_position-global_position).normalized()
+	SPEED += clutch*delta
+	if SPEED <= max_speed:
+		velocity = direction*SPEED
+	else:
+		velocity = direction*max_speed
+
+func look_at_player():
+	look_at(player.global_position)
+	rotation_degrees += 90
+
+func die():
+	queue_free()
+
+func fire(direction):
+	var offsets = [-16, 0, 16]
+	var dist = Vector2(-direction.y, direction.x)
+	var a : int = 0
+	for i in $Points.get_children():
+		var bullet = bulletscene.instantiate()
+		bullet.pos = player.global_position + dist * offsets[a]
+		bullet.global_position = i.global_position
+		area.add_child(bullet)
+		a+=1
+	$AnimationPlayer.play("Shoot")
+
+func _on_stop_area_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		state = states.STOP
+
+func _on_stop_area_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		state = states.FOLLOW
+
+func _on_timer_timeout() -> void:
+	can_shoot = true
