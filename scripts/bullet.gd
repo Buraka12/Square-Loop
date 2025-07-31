ee96bea11ddc1b5d181865ac6b1aaf50e70b067e
Metehan <mcgamer0700@gmail.com>
2025-07-31 11:35:16 +0300

    Düzenleme

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -2,12 +2,19 @@ extends RigidBody2D
 
 var pos : Vector2
 const SPEED : float = 500.0
+var dur : float = 6.0
+var time : float = 0.0
 
 func _ready() -> void:
 	var direction = (pos - global_position).normalized()
 	linear_velocity += direction * SPEED
 	rotation = get_angle_to(pos) + PI/2
 
+func _process(delta: float) -> void:
+	time+=delta
+	var ratio = time/dur
+	if ratio >= 1:
+		queue_free()
 
 func _on_hit_box_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Entity"):
--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -1,17 +1,28 @@
 extends CharacterBody2D
 
 
-const SPEED = 250.0
+var SPEED = 250.0
+
 var bulletscene : PackedScene = preload("res://scenes/player_bullet.tscn")
+var can_shoot : bool = true
+var fire_rate : float = 2
+var shooting : bool = false
 
 func _physics_process(delta: float) -> void:
 	
-	if Input.is_action_just_pressed("fire"):
+	if Input.is_action_pressed("fire") and can_shoot:
+		SPEED = 100.0
+		can_shoot = false
+		shooting = true
+		$Timer.start(1/fire_rate)
 		fire()
+	if Input.is_action_just_released("fire"):
+		SPEED = 250.0
+		shooting = false
 	
 	#mouse bakma
 	look_at(get_global_mouse_position())
-
+	
 	#sağ sol yukarı aşağı hareket
 	var directionx := Input.get_axis("left" , "right")
 	if directionx:
@@ -36,3 +47,6 @@ func fire():
 
 func die():
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+func _on_timer_timeout() -> void:
+	can_shoot = true
