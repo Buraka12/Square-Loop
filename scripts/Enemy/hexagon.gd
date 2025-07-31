372ee3fb0807e830f735aa016472c0e28f6518bc
Metehan <mcgamer0700@gmail.com>
2025-07-31 15:45:43 +0300

    Hexagon

--- scripts/Enemy/hexagon.gd
+++ scripts/Enemy/hexagon.gd
@@ -1,23 +1,28 @@
 extends CharacterBody2D
 
-var speed = 100
-var player_chase = false
-@onready var player: CharacterBody2D = $"."
+var max_speed : float = 300.0
+var speed : float = 0.0
+var clutch : float = 200.0
+var rot_speed : float = 700.0
+@onready var area : Node2D = $".."
+@onready var player: CharacterBody2D = area.find_child("Player")
 
 
 func _physics_process(delta: float) -> void:
-	if player_chase:
-		var direction = (player.position - position).normalized()
-		velocity = direction*speed
-		look_at(player.position)
-		move_and_slide()
-		
+	set_speed(delta)
+	rotation_degrees+=delta*rot_speed
+	move_and_slide()
 
-func _on_detection_body_entered(body: Node2D) -> void:
-	player = body
-	player_chase = true
+func set_speed(delta):
+	if speed < max_speed:
+		speed += delta * clutch
+	var direction = (player.global_position - global_position).normalized()
+	velocity = direction*speed
+
+func die():
+	queue_free()
 
 
-func _on_detection_body_exited(body: Node2D) -> void:
-	player = null
-	player_chase = false
+func _on_hit_box_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Entity"):
+		body.die()
