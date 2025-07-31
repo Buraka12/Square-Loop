a22a4603288cb5eec8f48dfd86e39326509aeb7e
Metehan <mcgamer0700@gmail.com>
2025-07-31 18:43:14 +0300

    Triangle Shoot Anim

--- scripts/Enemy/triangle.gd
+++ scripts/Enemy/triangle.gd
@@ -6,9 +6,12 @@ extends CharacterBody2D
 @onready var bulletscene : PackedScene = load("res://scenes/enemy_bullet.tscn")
 var can_shoot : bool = true
 var fire_rate : float = 1
+var tab : float = 400.0
 
 var health : float = 100
-const SPEED = 200.0
+var max_speed : float = 200.0
+var clutch : float = 200.0
+var SPEED : float = 0.0
 
 enum states {STOP,FOLLOW,FIRE}
 var state : states
@@ -19,20 +22,28 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	look_at_player()
 	if state != states.STOP:
-		follow_player()
+		follow_player(delta)
 	else:
+		SPEED = 0
 		velocity = Vector2(0,0)
 	
 	if can_shoot:
 		can_shoot = false
+		var direction = (player.global_position-global_position).normalized()
+		velocity = -direction*tab
+		SPEED = 0
 		$Timer.start(1/fire_rate)
 		fire()
 	
 	move_and_slide()
 
-func follow_player():
+func follow_player(delta):
 	var direction = (player.global_position-global_position).normalized()
-	velocity = direction*SPEED
+	SPEED += clutch*delta
+	if SPEED <= max_speed:
+		velocity = direction*SPEED
+	else:
+		velocity = direction*max_speed
 
 func look_at_player():
 	look_at(player.global_position)
@@ -44,7 +55,8 @@ func die():
 func fire():
 	var bullet = bulletscene.instantiate()
 	bullet.pos = player.global_position
-	bullet.global_position = global_position
+	bullet.global_position = $Marker2D.global_position
+	$AnimationPlayer.play("Shoot")
 	area.add_child(bullet)
 
 func _on_stop_area_body_entered(body: Node2D) -> void:
